abd48b890f78549857facdf7f31a9f27e7cd664d
guillem-martinez <guillem.martinezar@e-campus.uab.cat>
2021-10-21 11:23:12 +0200

    Clock

--- code/src/Clock.java
+++ code/src/Clock.java
@@ -1,3 +1,34 @@
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Clock {
+
+  private static LocalDateTime dateTime;
+  private static Timer timer;
+  private static Clock instance = null;
+
+  private Clock(){
+    timer = new Timer();
+  }
+
+  public static void initializeClock(){
+    timer = new Timer();
+    TimerTask repeatTask = new TimerTask() {
+      @Override
+      public void run() {
+        dateTime = LocalDateTime.now();
+        System.out.println(dateTime);
+      }
+    };
+  }
+
+  public void stop(){
+    timer.cancel();
+  }
+
+  public LocalDateTime getDateTime(){
+    return dateTime;
+  }
 
 }
