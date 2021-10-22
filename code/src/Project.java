a72c0a51e6a6f446a84e2d8fe35080608e7b93ba
guillem-martinez <guillem.martinezar@e-campus.uab.cat>
2021-10-22 09:27:05 +0200

    Update

--- code/src/Project.java
+++ code/src/Project.java
@@ -1,12 +1,23 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class Project extends Event{
 
 
-  private List<Event> elements;
+  private LinkedList<Event> elements; //preguntar profe
   public String proj_name;
 
   public void add(Event e){
     elements.add(e);
   }
+
+  public void setProj_name(String name){
+    proj_name = name;
+  }
+
+  private void delete(String name){
+
+  }
+
+
 }
--- code/src/Task.java
+++ code/src/Task.java
@@ -3,4 +3,8 @@ import java.util.List;
 public class Task extends Event {
 
   List<Interval> task_duration;
+
+  public void setTaskName(String name){
+    this.name = name;
+  };
 }
